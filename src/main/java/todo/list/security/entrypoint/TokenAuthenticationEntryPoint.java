da17de24321209aeb166f68fe3f5260d517c2fb8
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:21:46 +0900

    feat : 접근 거부가 되었을때에 대한 처리

---
+++ src/main/java/todo/list/security/entrypoint/TokenAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package todo.list.security.entrypoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import todo.list.api.dto.response.CommonResponse;
+import todo.list.domain.account.log.ErrorLog;
+import todo.list.domain.account.log.ErrorLogRepository;
+
+@Component
+@RequiredArgsConstructor
+public class TokenAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+    private final ErrorLogRepository errorLogRepository;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException authException) throws IOException, ServletException {
+        errorLogRepository.save(ErrorLog.create(
+            401, request.getMethod(), "UNAUTHORIZED", authException.getMessage(),
+            request.getRequestURI().toString()
+        ));
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(401);
+        response.getWriter().write(
+            objectMapper.writeValueAsString(
+                CommonResponse.of(HttpStatus.UNAUTHORIZED, authException.getMessage()))
+        );
+    }
+}
