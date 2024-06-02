9235640de0d05072c9b64e8f71e2041dcb9e031c
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:24:07 +0900

    feat : 토큰 정보 응답객체

---
+++ src/main/java/todo/list/token/response/TokenResponse.java
@@ -0,0 +1,15 @@
+package todo.list.token.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+public record TokenResponse(
+    Long id,
+    String grantType,
+    String accessToken,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime accessTokenExpireAt,
+    String refreshToken,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime refreshTokenExpireAt
+) { }
