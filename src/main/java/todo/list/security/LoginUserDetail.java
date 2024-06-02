89eb2902ccb37e4c3310651f40f4614ad138d131
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:23:51 +0900

    feat : 로그인 한 사용자 정보 담기위한 객체

---
+++ src/main/java/todo/list/security/LoginUserDetail.java
@@ -0,0 +1,35 @@
+package todo.list.security;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@AllArgsConstructor
+public class LoginUserDetail implements UserDetails {
+
+    private Long id;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (role == null) {
+            return List.of();
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return id.toString();
+    }
+}
