91588c3927551a782138a5baafd9dceae1429af4
cousim46 <wjdghldns24@naver.com>
2024-06-02 21:35:47 +0900

    feat : 메서드 쿼리 추가 - 아이디 존재 여부 - 닉네임 존재 여부

--- src/main/java/todo/list/domain/account/AccountRepository.java
+++ src/main/java/todo/list/domain/account/AccountRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickName);
 }
