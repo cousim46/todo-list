f064347f8442b98121a379d4f71b44f4e749edf0
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:06:48 +0900

    feat : 토큰 레파지토리 생성

---
+++ src/main/java/todo/list/token/TokenRepository.java
@@ -0,0 +1,14 @@
+package todo.list.token;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import todo.list.domain.token.Token;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByAccountId(Long accountId);
+
+    @Query("SELECT t FROM Token t JOIN FETCH t.account WHERE t.access = :access")
+    Optional<Token> findTokenLoginInfo(@Param("access") String access);
+}
