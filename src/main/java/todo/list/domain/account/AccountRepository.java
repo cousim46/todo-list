4aa74d58f1384c96b5d8921d48b83c24762ea5d8
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:04:14 +0900

    feat : 로그인 아이디로 회원을 조회하는 메서드 쿼리 추가

--- src/main/java/todo/list/domain/account/AccountRepository.java
+++ src/main/java/todo/list/domain/account/AccountRepository.java
@@ -1,9 +1,11 @@
 package todo.list.domain.account;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     boolean existsByLoginId(String loginId);
     boolean existsByNickname(String nickName);
+    Optional<Account> findByLoginId(String loginId);
 }
