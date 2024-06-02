9a504cfa9515b3498e927fa6c9b270c9d49cd25b
cousim46 <wjdghldns24@naver.com>
2024-06-03 01:05:06 +0900

    feat : 필터, entryPoint 설정

--- src/main/java/todo/list/config/SecurityConfig.java
+++ src/main/java/todo/list/config/SecurityConfig.java
@@ -1,22 +1,33 @@
 package todo.list.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import todo.list.security.entrypoint.TokenAuthenticationEntryPoint;
+import todo.list.security.filter.LoginFilter;
+import todo.list.security.handler.JwtAccessDeninedHandler;
+import todo.list.token.TokenProvider;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAccessDeninedHandler jwtAccessDeninedHandler;
+    private final TokenAuthenticationEntryPoint tokenAuthenticationEntryPoint;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -29,6 +40,11 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(Customizer.withDefaults())
+            .addFilterBefore(new LoginFilter(tokenProvider, tokenAuthenticationEntryPoint),
+                UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling((exceptionConfig) -> exceptionConfig.authenticationEntryPoint(
+                tokenAuthenticationEntryPoint).accessDeniedHandler(jwtAccessDeninedHandler)
+            )
             .authorizeHttpRequests(authorizeRequest ->
                 authorizeRequest
                     .anyRequest().permitAll()
