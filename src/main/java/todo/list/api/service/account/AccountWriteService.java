4216e952c5f8db0ff6636e65a6307997ea46b1bf
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:04:52 +0900

    fix : 메서드명 변경

--- src/main/java/todo/list/api/service/account/AccountWriteService.java
+++ src/main/java/todo/list/api/service/account/AccountWriteService.java
@@ -1,6 +1,9 @@
 package todo.list.api.service.account;
 
 
+import static todo.list.exception.ErrorCode.NOT_MATCH_PASSWORD_OR_ID;
+
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import todo.list.api.service.annotaion.WriteService;
@@ -8,6 +11,8 @@ import todo.list.domain.account.Account;
 import todo.list.domain.account.AccountRepository;
 import todo.list.exception.ErrorCode;
 import todo.list.exception.TodoListException;
+import todo.list.token.TokenProvider;
+import todo.list.token.response.TokenResponse;
 
 @RequiredArgsConstructor
 @WriteService
@@ -15,13 +20,12 @@ public class AccountWriteService {
     private final BCryptPasswordEncoder encoder;
     private final AccountRepository accountRepository;
 
-    public void singUp(String loginId, String nickname, String password, String salt) {
+    public void signUp(String loginId, String nickname, String password, String salt) {
         duplicateValidation(loginId, nickname);
         String encodeSalt = encoder.encode(salt);
         String encodePassword = encoder.encode(password + encodeSalt);
-        accountRepository.save(Account.signUp(nickname,loginId, encodePassword, encodeSalt));
+        accountRepository.save(Account.signUp(nickname, loginId, encodePassword, encodeSalt));
     }
-
     private void duplicateValidation(String loginId, String nickname) {
         if(accountRepository.existsByLoginId(loginId)) {
             throw new TodoListException(ErrorCode.DUPLICATE_LOGIN_ID);
