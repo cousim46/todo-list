315a71590d73d4c5a77af02813e35fed77c163be
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:37:34 +0900

    feat : 성공, 실패, 서버에러시 응답 메서드 추가

--- src/main/java/todo/list/api/dto/response/CommonResponse.java
+++ src/main/java/todo/list/api/dto/response/CommonResponse.java
@@ -20,6 +20,18 @@ public class CommonResponse<T> {
     public static <T>CommonResponse<T> of(HttpStatus status, T data) {
         return new CommonResponse<>(status, data);
     }
+
+    public static <T> CommonResponse<T> badRequest(T errorData) {
+        return of(HttpStatus.BAD_REQUEST, errorData);
+    }
+
+    public static <T> CommonResponse<T> ok(T data) {
+        return of(HttpStatus.OK, data);
+    }
+    public static <T>CommonResponse<T> serverError(T data) {
+        return of(HttpStatus.INTERNAL_SERVER_ERROR, data);
+    }
+
 }
 
 
