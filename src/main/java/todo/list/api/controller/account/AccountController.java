ab680c46d9e4e1bbc65a696fd91ad0c045374ff3
cousim46 <wjdghldns24@naver.com>
2024-06-03 01:05:52 +0900

    feat : 회원 탈퇴 API

--- src/main/java/todo/list/api/controller/account/AccountController.java
+++ src/main/java/todo/list/api/controller/account/AccountController.java
@@ -3,6 +3,8 @@ package todo.list.api.controller.account;
 import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import todo.list.api.controller.account.dto.request.AccountSignUpRequest;
 import todo.list.api.controller.account.dto.request.LoginRequest;
 import todo.list.api.controller.account.dto.response.TokenInfoResponse;
 import todo.list.api.controller.account.helper.PasswordHelper;
+import todo.list.api.controller.annotation.LoginUser;
+import todo.list.api.controller.dto.request.LoginUserInfo;
 import todo.list.api.dto.response.CommonResponse;
 import todo.list.api.service.account.AccountWriteService;
 
@@ -31,5 +35,11 @@ public class AccountController {
     public CommonResponse<TokenInfoResponse> login(@Valid @RequestBody LoginRequest request) {
         return CommonResponse.ok(TokenInfoResponse.of(
             accountWriteService.login(request.loginId(), request.password(), LocalDateTime.now())));
+    }
+
+    @DeleteMapping
+    @Secured("ROLE_USER")
+    public void withdraw(@LoginUser LoginUserInfo loginUserInfo) {
+        accountWriteService.withdraw(loginUserInfo.id());
     }
 }
