80dba10604e2b48d7cfe8f952b159afcdb7d1ddd
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:37:46 +0900

    feat : 로그인 API

--- src/main/java/todo/list/api/controller/account/AccountController.java
+++ src/main/java/todo/list/api/controller/account/AccountController.java
@@ -1,25 +1,35 @@
 package todo.list.api.controller.account;
 
 import jakarta.validation.Valid;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todo.list.api.controller.account.dto.request.AccountSignUpRequest;
+import todo.list.api.controller.account.dto.request.LoginRequest;
+import todo.list.api.controller.account.dto.response.TokenInfoResponse;
 import todo.list.api.controller.account.helper.PasswordHelper;
+import todo.list.api.dto.response.CommonResponse;
 import todo.list.api.service.account.AccountWriteService;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/accounts")
 @RestController
 public class AccountController {
+
     private final AccountWriteService accountWriteService;
 
     @PostMapping
     public void signUp(@Valid @RequestBody AccountSignUpRequest request) {
-        accountWriteService.signUp(request.loginId(),request.nickname(),request.password(),
+        accountWriteService.signUp(request.loginId(), request.nickname(), request.password(),
             PasswordHelper.createSalt());
     }
 
+    @PostMapping("/login")
+    public CommonResponse<TokenInfoResponse> login(@Valid @RequestBody LoginRequest request) {
+        return CommonResponse.ok(TokenInfoResponse.of(
+            accountWriteService.login(request.loginId(), request.password(), LocalDateTime.now())));
+    }
 }
