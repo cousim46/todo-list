2939921fce8d9f8f9d9d06894965d3b97472cebd
cousim46 <wjdghldns24@naver.com>
2024-06-02 19:52:25 +0900

    feat : auditing 설정

---
+++ src/main/java/todo/list/config/AuditingConfig.java
@@ -0,0 +1,10 @@
+package todo.list.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditingConfig {
+
+}
