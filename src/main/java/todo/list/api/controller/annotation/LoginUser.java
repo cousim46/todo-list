e7078629d9568387dfd8fcb64ce68d57e50741d6
cousim46 <wjdghldns24@naver.com>
2024-06-03 00:24:49 +0900

    feat : 파라미터에 로그인 정보를 담기 위한 커스텀 어노테이션 생성

---
+++ src/main/java/todo/list/api/controller/annotation/LoginUser.java
@@ -0,0 +1,11 @@
+package todo.list.api.controller.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.ANNOTATION_TYPE,ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
