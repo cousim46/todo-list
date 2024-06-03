6f2c6210d4e30b5c2c08dde61112f5764a88e872
cousim46 <wjdghldns24@naver.com>
2024-06-03 20:37:07 +0900

    test : Todo 상태 변경을 위한 비즈니스 로직 테스트 - 성공 - 진행 중 외의 상태에서 대기 상태로 변경 할 때 - 변경하려는 회원이 존재하지 않을 경우 - 변경하려는 회원이 자기의 Todo가 아닌 다른 Todo를 변경 할 경우

--- src/test/java/todo/list/api/service/todolist/TodoListWriteServiceTest.java
+++ src/test/java/todo/list/api/service/todolist/TodoListWriteServiceTest.java
@@ -3,6 +3,8 @@ package todo.list.api.service.todolist;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static todo.list.domain.todolist.enums.TodoStatus.IN_PROGRESS;
+import static todo.list.domain.todolist.enums.TodoStatus.PENDING;
 
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
@@ -104,6 +106,101 @@ class TodoListWriteServiceTest {
 
         //then
         assertThat(todoListException.getExceptionMessage()).isEqualTo("회원이 존재하지 않습니다.");
+        assertThat(todoListException.getExceptionStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+    @DisplayName("Todo의 상태값을 변경 할 수 있다.")
+    @Test
+    void updateStatus() {
+        //given
+        String nickname = "nickname";
+        String loginId = "loginId";
+        String password = "password";
+        String salt = "salt";
+        Account savedAccount = accountRepository.save(
+            Account.signUp(nickname, loginId, password, salt));
+
+        String title = "title";
+        String content = "content";
+        Todo savedTodo = todoRepository.save(Todo.create(title, content, savedAccount));
+
+        TodoStatus beforeStatus = savedTodo.getStatus();
+
+        //when
+        todoListWriteService.updateStatus(savedAccount.getId(), savedTodo.getId(), IN_PROGRESS);
+
+        //then
+        Todo todo = todoRepository.findById(savedTodo.getId()).get();
+        assertThat(todo.getStatus()).isNotEqualTo(beforeStatus);
+        assertThat(todo.getStatus()).isEqualTo(IN_PROGRESS);
+    }
+    @DisplayName("Todo의 상태값을 PENDING으로 변경할 때 IN_PROGRESS 상태가 아닐 경우에 예외가 발생한다.")
+    @Test
+    void occurNotStatusInProgressChangePendingException() {
+        //given
+        String nickname = "nickname";
+        String loginId = "loginId";
+        String password = "password";
+        String salt = "salt";
+        Account savedAccount = accountRepository.save(
+            Account.signUp(nickname, loginId, password, salt));
+
+        String title = "title";
+        String content = "content";
+        Todo savedTodo = todoRepository.save(Todo.create(title, content, savedAccount));
+
+        //when
+        TodoListException todoListException = assertThrows(TodoListException.class,
+            () -> todoListWriteService.updateStatus(savedAccount.getId(), savedTodo.getId(),
+                PENDING));
+
+        //then
+        assertThat(todoListException.getExceptionMessage()).isEqualTo("대기 상태는 진행 상태일때만 상태 변경이 가능합니다.");
+        assertThat(todoListException.getExceptionStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+    @DisplayName("상태 변경하려는 사용자가 존재하지 않으면 예외가 발생한다.")
+    @Test
+    void occurNotExistAccountException() {
+        //given
+        String nickname = "nickname";
+        String loginId = "loginId";
+        String password = "password";
+        String salt = "salt";
+        Account savedAccount = accountRepository.save(
+            Account.signUp(nickname, loginId, password, salt));
+        String title = "title";
+        String content = "content";
+        Todo savedTodo = todoRepository.save(Todo.create(title, content, savedAccount));
+        Long requestAccountId = -1L;
+
+        //when
+        TodoListException todoListException = assertThrows(TodoListException.class,
+            () -> todoListWriteService.updateStatus(requestAccountId, savedTodo.getId(),
+                IN_PROGRESS));
+
+        //then
+        assertThat(todoListException.getExceptionMessage()).isEqualTo("회원이 존재하지 않습니다.");
+        assertThat(todoListException.getExceptionStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @DisplayName("상태 변경하려는 회원이 회원의 것이 아닌 Todo를 변경하려고 할때 예외가 발생한다.")
+    @Test
+    void occurNotExistTodoException() {
+        String nickname = "nickname";
+        String loginId = "loginId";
+        String password = "password";
+        String salt = "salt";
+        Account savedAccount = accountRepository.save(
+            Account.signUp(nickname, loginId, password, salt));
+        Long requestTodoId = -1L;
+
+
+        //when
+        TodoListException todoListException = assertThrows(TodoListException.class,
+            () -> todoListWriteService.updateStatus(savedAccount.getId(), requestTodoId,
+                IN_PROGRESS));
+
+        //then
+        assertThat(todoListException.getExceptionMessage()).isEqualTo("존재하지 않는 Todo입니다.");
         assertThat(todoListException.getExceptionStatus()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
