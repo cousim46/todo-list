e175a474a3ba416335a8d9d794d137315947bb90
cousim46 <wjdghldns24@naver.com>
2024-06-03 16:08:44 +0900

    feat : TODO 상태에 대한 비즈니스 계층의 enum 추가

---
+++ src/main/java/todo/list/api/service/todolist/dto/enums/TodoStatus.java
@@ -0,0 +1,12 @@
+package todo.list.api.service.todolist.dto.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TodoStatus {
+    TODO("할 일"), IN_PROGRESS("진행중"),DONE("완료"), PENDING("대기");
+
+    private final String content;
+}
