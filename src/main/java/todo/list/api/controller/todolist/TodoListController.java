24117bd2ba1dccaca6cfa4b3b4296ceeb688f7e7
cousim46 <wjdghldns24@naver.com>
2024-06-03 16:30:38 +0900

    feat : 조회에 대한 API

--- src/main/java/todo/list/api/controller/todolist/TodoListController.java
+++ src/main/java/todo/list/api/controller/todolist/TodoListController.java
@@ -1,16 +1,22 @@
 package todo.list.api.controller.todolist;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todo.list.api.controller.annotation.LoginUser;
 import todo.list.api.controller.dto.request.LoginUserInfo;
+import todo.list.api.controller.dto.response.CommonResponse;
+import todo.list.api.controller.todolist.dto.request.SearchRequest;
 import todo.list.api.controller.todolist.dto.request.TodoCreateRequest;
+import todo.list.api.service.todolist.TodoListReadService;
 import todo.list.api.service.todolist.TodoListWriteService;
+import todo.list.api.service.todolist.dto.response.TodoListResponse;
 
 @RequiredArgsConstructor
 @Secured("ROLE_USER")
@@ -19,10 +25,19 @@ import todo.list.api.service.todolist.TodoListWriteService;
 public class TodoListController {
 
     private final TodoListWriteService todoListWriteService;
+    private final TodoListReadService todoListReadService;
 
     @PostMapping
     public void create(@LoginUser LoginUserInfo loginUserInfo,
         @Valid @RequestBody TodoCreateRequest request) {
         todoListWriteService.create(loginUserInfo.id(), request.title(), request.title());
+    }
+
+    @GetMapping
+    public CommonResponse<List<TodoListResponse>> findAllOrRecent(@LoginUser LoginUserInfo loginUserInfo,
+        @Valid SearchRequest request) {
+        return CommonResponse.ok(
+            todoListReadService.findAllOrRecent(request.type(), loginUserInfo.id())
+        );
     }
 }
