95c65c2d7674ad06823de77d24a4b26ce04d6eee
cousim46 <wjdghldns24@naver.com>
2024-06-03 14:01:55 +0900

    feat : Todo 생성 테스트

---
+++ src/test/java/todo/list/domain/todolist/TodoTest.java
@@ -0,0 +1,54 @@
+package todo.list.domain.todolist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import todo.list.domain.account.Account;
+import todo.list.domain.todolist.enums.TodoStatus;
+
+class TodoTest {
+
+    @DisplayName("회원은 제목, 내용으로 Todo 생성한다.")
+    @Test
+    void create() {
+        //given
+        String nickname = "nickname";
+        String loginId = "loginId";
+        String password = "password";
+        String salt = "salt";
+        Account account = Account.signUp(nickname, loginId, password, salt);
+
+        String title = "title";
+        String content = "content";
+
+        //when
+        Todo todo = Todo.create(title, content, account);
+
+        //then
+        assertThat(todo.getContent()).isEqualTo(content);
+        assertThat(todo.getTitle()).isEqualTo(title);
+        assertThat(todo.getAccount()).isEqualTo(account);
+    }
+
+    @DisplayName("Todo가 생성되면 기본으로 '할 일' 상태값을 갖는다.")
+    @Test
+    void createTodoListBaseStatusTODO() {
+        //given
+        String nickname = "nickname";
+        String loginId = "loginId";
+        String password = "password";
+        String salt = "salt";
+        Account account = Account.signUp(nickname, loginId, password, salt);
+
+        String title = "title";
+        String content = "content";
+
+        //when
+        Todo todo = Todo.create(title, content, account);
+
+        //then
+        assertThat(todo.getStatus()).isEqualTo(TodoStatus.TODO);
+    }
+
+}
