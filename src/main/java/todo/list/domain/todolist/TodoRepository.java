404563cef4fb250b00c4b3639acd5256798f128c
cousim46 <wjdghldns24@naver.com>
2024-06-03 16:09:28 +0900

    feat : Todo 레파지토리 - 최신 등록한 Todo 조회 - 등록한 Todo 전체 조회

--- src/main/java/todo/list/domain/todolist/TodoRepository.java
+++ src/main/java/todo/list/domain/todolist/TodoRepository.java
@@ -1,7 +1,16 @@
 package todo.list.domain.todolist;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    @Query(value = "SELECT * FROM todo WHERE account_id = :accountId ORDER BY updated_at DESC LIMIT 1", nativeQuery = true)
+    Optional<Todo> findRecent(@Param("accountId") Long accountId);
+
+    List<Todo> findAllByAccountIdOrderByUpdatedAtDesc(@Param("accountId") Long accountId);
 
 }
