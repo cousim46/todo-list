108fffee93a72bb317e6226fd8b38712cc6fa23f
cousim46 <wjdghldns24@naver.com>
2024-06-04 12:33:34 +0900

    fix : 메시지 삭제

--- src/main/java/todo/list/api/controller/account/dto/request/RefreshRequest.java
+++ src/main/java/todo/list/api/controller/account/dto/request/RefreshRequest.java
@@ -3,7 +3,7 @@ package todo.list.api.controller.account.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefreshRequest(
-    @NotBlank(message = "리프레쉬 토큰은 필수 값입니다.")
+    @NotBlank
     String token
 ) {
 
